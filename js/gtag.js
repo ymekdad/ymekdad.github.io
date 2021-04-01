544d9a760cbb59a4c6b7e8451cf2a9386c76f0ae
Yassine Mekdad <mekdadyassine@gmail.com>
2021-04-02 00:10:53 +0100

    Update gtag.js

--- js/gtag.js
+++ js/gtag.js
@@ -10,23 +10,11 @@ function loadScriptAsync(scriptSrc, callback) {
 }
 
 /* This is the part where you call the above defined function and "call back" your code which gets executed after the script has loaded */
-loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=G-0RNQS33SX8', function(){
+loadScriptAsync('https://www.googletagmanager.com/gtag/js?id=GTM-NTCL97R', function(){
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-0RNQS33SX8', { 'anonymize_ip': true });
+    gtag('config', 'GTM-NTCL97R', { 'anonymize_ip': true });
 })
 
-
-/*
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-87653100-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-87653100-1', { 'anonymize_ip': true });
-  </script>
-*/
